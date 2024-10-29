808b279d2f94c4f59838e1ee0b9a8cb25a131370
ahendest <goz.m.abdurrahman23@stud.rau.ro>
2024-10-29 10:33:26 +0200

    cards updated, switch added

--- src/Home/LandingPage/Cards/Cards.tsx
+++ src/Home/LandingPage/Cards/Cards.tsx
@@ -7,8 +7,6 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-
-
 function Cards() {
   return (
     <main>
@@ -18,21 +16,24 @@ function Cards() {
         <p>
           Dui euismod iaculis libero, aliquet vitae et elementum porttitor. Eleifend mi tristique condimentum congue fusce nunc, donec magnis commodo.
         </p>
-        <RequestButton/>
+        <RequestButton />
       </div>
 
 
-      <Swiper direction="horizontal" // Change to "vertical" if you want vertical swipe
-      loop={true}
-      pagination={{ clickable: true }}
-      navigation={{ nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' }}
-      scrollbar={{ draggable: true }}
-      spaceBetween={20} // Adjust spacing between slides if needed
-      slidesPerView={1} // Display one card at a time; adjust for responsive needs
+      <Swiper className='card-big-container'
+        direction="horizontal"
+        loop={true}
+        pagination={{ el: ".swiper-pagination", clickable: true }}
+        navigation={{ nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' }}
+        scrollbar={{ draggable: true }}
+        spaceBetween={20}
+        slidesPerView={1}
+        speed={800}
+        slideToClickedSlide={true}
       >
-        <div className='card'>
-          <SwiperSlide>
-            <div>
+        <div className='card-middle-container'> 
+          <SwiperSlide className='swiper-slide 1'>
+            <div className='card-container'>
               <img src="./assets/cloud.svg" alt="" />
               <p>
                 Purus maecenas quis elit eu, aliquet. Tellus porttitor ut sollicitudin sit non fringilla. Quam nunc volutpat senectus neque eget amet pharetra, euismod. Tempus, nunc, molestie imperdiet curabitur commodo euismod.
@@ -46,11 +47,12 @@ function Cards() {
               </div>
             </div>
           </SwiperSlide>
-          <SwiperSlide>
-            <div>
+
+          <SwiperSlide className='swiper-slide 2'>
+            <div className='card-container'>
               <img src="./assets/star.svg" alt="" />
               <p>
-              Vehicula sit sit pharetra bibendum ut risus accumsan. Purus, in metus, enim, ipsum morbi euismod pellentesque. Mattis pharetra accumsan eget est mi enim, id. Sit quam tortor eu tellus non, in euismod integer.
+                Vehicula sit sit pharetra bibendum ut risus accumsan. Purus, in metus, enim, ipsum morbi euismod pellentesque. Mattis pharetra accumsan eget est mi enim, id. Sit quam tortor eu tellus non, in euismod integer.
               </p>
               <div className='user-card'>
                 <img src="./assets/rwanda.jpeg" alt="" />
@@ -61,11 +63,12 @@ function Cards() {
               </div>
             </div>
           </SwiperSlide>
-          <SwiperSlide>
-            <div>
+
+          <SwiperSlide className='swiper-slide 3'>
+            <div className='card-container'>
               <img src="./assets/tower.svg" alt="" />
               <p>
-              Viverra lacus suspendisse elit, adipiscing orci, non turpis etiam sapien. Viverra blandit sem neque pretium. Duis enim semper fermentum consequat aenean libero. Blandit porta leo condimentum dolor, nisi, aliquet ante laoreet.
+                Viverra lacus suspendisse elit, adipiscing orci, non turpis etiam sapien. Viverra blandit sem neque pretium. Duis enim semper fermentum consequat aenean libero. Blandit porta leo condimentum dolor, nisi, aliquet ante laoreet.
               </p>
               <div className='user-card'>
                 <img src="./assets/rwanda.jpeg" alt="" />
@@ -76,11 +79,12 @@ function Cards() {
               </div>
             </div>
           </SwiperSlide>
-          <SwiperSlide>
-            <div>
+
+          <SwiperSlide className='swiper-slide 4'>
+            <div className='card-container'>
               <img src="./assets/timer.svg" alt="" />
               <p>
-              Hendrerit augue ut nec, senectus quis integer netus. Sagittis fusce rhoncus magnis habitant amet amet. Egestas amet habitasse amet risus tellus ornare. Hendrerit augue ut nec, senectus. Mauris egestas feugiat leo vitae praesent neque, et.
+                Hendrerit augue ut nec, senectus quis integer netus. Sagittis fusce rhoncus magnis habitant amet amet. Egestas amet habitasse amet risus tellus ornare. Hendrerit augue ut nec, senectus. Mauris egestas feugiat leo vitae praesent neque, et.
               </p>
               <div className='user-card'>
                 <img src="./assets/rwanda.jpeg" alt="" />
@@ -92,6 +96,13 @@ function Cards() {
             </div>
           </SwiperSlide>
         </div>
+
+        <div className='swiper-pagination'></div>
+        <div className='buttons'>
+          <div className='swiper-button-next'></div>
+          <div className='swiper-button-prev'></div>
+        </div>
+        
       </Swiper>
     </main>
   )
